a18b5c3b5533f282f0a32fdb5586ea2ca421f4db
Tharaka Priyadarshana <sunbrightlanka@gmail.com>
2024-01-12 23:47:10 +0530

    first chnage

---
+++ app/src/main/java/com/example/sprinklessbakersapp/Activity/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.example.sprinklessbakersapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sprinklessbakersapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    public String TAG="Sprinkles Bakers";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+    }
+}
